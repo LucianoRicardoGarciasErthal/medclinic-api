03e54d7cef25d4e5bf7a370285c9b0194bc88965
Luciano Ricardo Garcias Erthal <luciano.r.garcias.e@gmail.com>
2026-09-13 08:15:12 -0300

    fix: usa commonjs e node10 para compatibilidade com ts-node

--- src/config/database.ts
+++ src/config/database.ts
@@ -1,7 +1,7 @@
 import 'dotenv/config';
 import 'reflect-metadata';
 import { DataSource } from 'typeorm';
-import { User } from '../entities/User.js';
+import { User } from '../entities/User';
 
 export const AppDataSource = new DataSource({
   type: 'postgres',
